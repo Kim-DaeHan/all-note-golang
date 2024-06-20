c09a41b7f4029aa651b3fa9634360ea4c9cc84f8
DaeHan Kim <kjs50458281@gmail.com>
2024-06-20 21:58:13 +0900

    add ,omitempty

--- dto/todo_create_dto.go
+++ dto/todo_create_dto.go
@@ -7,9 +7,9 @@ import "time"
 type TodoCreateDTO struct {
 	Task       string    `json:"task"`
 	Status     string    `json:"status"`
-	Project    string    `json:"project"`
+	Project    string    `json:"project,omitempty"`
 	StartDt    time.Time `json:"start_dt"`
 	EndDt      time.Time `json:"end_dt"`
 	User       string    `json:"user"`
-	Department string    `json:"department"`
+	Department string    `json:"department,omitempty"`
 } //@name TodoCreateDTO
--- dto/todo_update_dto.go
+++ dto/todo_update_dto.go
@@ -5,10 +5,10 @@ import "time"
 // TodoUpdateDTO info
 // @Description Todo information update dto
 type TodoUpdateDTO struct {
-	Task       string    `json:"task"`
-	Status     string    `json:"status"`
-	Project    string    `json:"project"`
-	StartDt    time.Time `json:"start_dt"`
-	EndDt      time.Time `json:"end_dt"`
-	Department string    `json:"department"`
+	Task       string    `json:"task,omitempty"`
+	Status     string    `json:"status,omitempty"`
+	Project    string    `json:"project,omitempty"`
+	StartDt    time.Time `json:"start_dt,omitempty"`
+	EndDt      time.Time `json:"end_dt,omitempty"`
+	Department string    `json:"department,omitempty"`
 } //@name TodoUpdateDTO
